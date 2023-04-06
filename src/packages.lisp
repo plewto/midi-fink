c0d11658eb9205c87acf9340907ba031fe28c0c6
Steven Jones <plewto.gmail.com>
2023-04-06 12:19:09 -0500

    Adds MIDI message constructors to package export list.

--- src/packages.lisp
+++ src/packages.lisp
@@ -9,9 +9,12 @@
 (in-package :midi-fink)
 
 (export '(channel-message-p
+	  channel-pressure
 	  channel-pressure-p
+	  control-change
 	  control-change-p
 	  dump-events
+	  end-system-exclusive
 	  end-system-exclusive-p
 	  event
 	  eventlist-p
@@ -20,27 +23,41 @@
 	  keyed-message-p
 	  merge-events
 	  messagep
+	  meta-channel-prefix
 	  meta-channel-prefix-p
+	  meta-end-of-track
 	  meta-end-of-track-p
+	  meta-keysig
 	  meta-keysig-accidentals
 	  meta-keysig-minor-p
 	  meta-keysig-p
+	  meta-sequence-number
 	  meta-sequence-number-p
+	  meta-sequencer
 	  meta-sequencer-p
+	  meta-smpte
 	  meta-smpte-p
+	  meta-tempo
 	  meta-tempo-p
+	  meta-text
 	  meta-text-p
 	  meta-timesig-p
 	  metap
 	  midi-clone
+	  midifile
 	  midifile-p
+	  note-off
 	  note-off-p
+	  note-on
 	  note-on-p
+	  pitch-bend
 	  pitch-bend-p
+	  polyphonic-pressure
 	  polyphonic-pressure-p
 	  precedence
+	  program-change
 	  program-change-p
-	  push-event
+	  push-event!
 	  set-default-division
 	  set-default-tempo
 	  system-exclusive
